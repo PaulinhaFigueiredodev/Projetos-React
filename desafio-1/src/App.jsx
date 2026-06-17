912e647155323f53961b7936fb569856f2fe826f
Paula Figueiredo <paulafigueiredo36@gmail.com>
2026-06-17 15:23:23 -0300

    feat: adiciona label e textarea para comentario

--- desafio-1/src/App.jsx
+++ desafio-1/src/App.jsx
@@ -4,6 +4,8 @@ function App() {
       <h1>Comentários</h1>
       <section>
         <h2>Adicionar comentário</h2>
+        <label htmlFor="comentario">Comentário</label>
+        <textarea id="comentario"></textarea>
       </section>
       <section>
         <h2>Comentários adicionados</h2>
