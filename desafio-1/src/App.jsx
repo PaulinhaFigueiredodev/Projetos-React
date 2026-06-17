5d7d1ea7e23eb7124587315ad210c072a8966111
Paula Figueiredo <paulafigueiredo36@gmail.com>
2026-06-17 01:47:21 -0300

    feat: cria estrutura semantica

--- desafio-1/src/App.jsx
+++ desafio-1/src/App.jsx
@@ -1,5 +1,15 @@
 function App() {
-  return "Hello world"
+  return (
+    <main>
+      <h1>Comentários</h1>
+      <section>
+        <h2>Adicionar comentário</h2>
+      </section>
+      <section>
+        <h2>Comentários adicionados</h2>
+      </section>
+    </main>
+  )       
 }
 
 export default App
