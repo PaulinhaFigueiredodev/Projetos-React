f22952b8e38db4576ad0f5e60f6c7cd5735bb127
Paula Figueiredo <paulafigueiredo36@gmail.com>
2026-06-17 17:39:42 -0300

    feat: adiciona state management for comment input in App component

--- desafio-1/src/App.jsx
+++ desafio-1/src/App.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react"
+
 function App() {
+
+  const [textoComentario, setTextoComentario] = useState("")
+
   return (
     <main>
       <h1>Comentários</h1>
+
       <section>
         <h2>Adicionar comentário</h2>
+
         <label htmlFor="comentario">Comentário</label>
-        <textarea id="comentario"></textarea>
+
+        <textarea id="comentario" 
+          value = {textoComentario}
+          onChange = {(e) => setTextoComentario(e.target.value)}></textarea>
       </section>
+
       <section>
+
         <h2>Comentários adicionados</h2>
+
       </section>
     </main>
   )       
