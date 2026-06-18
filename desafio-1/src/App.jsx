b5fa0ae5e28284a5b8d646a0e68d2d47c386c1f7
Paula Figueiredo <paulafigueiredo36@gmail.com>
2026-06-18 06:35:35 -0300

    feat: implementa adicao de comentarios

--- desafio-1/src/App.jsx
+++ desafio-1/src/App.jsx
@@ -1,8 +1,28 @@
 import { useState } from "react"
+/* O useState cria uma memoria dentro do componente
+  Essa memória serve para guardar um valor que pode mudar com o tempo.
+*/
 
 function App() {
 
-  const [textoComentario, setTextoComentario] = useState("")
+  const [textoComentario, setTextoComentario] = useState("");
+  const [listaComentarios, setListaComentarios] = useState([]);
+
+  /**
+      Quando adicionarComentario for chamada:
+        crie uma nova lista com tudo que já existia em listaComentarios
+        adicione textoComentario no final
+        salve essa nova lista em listaComentarios
+        limpe textoComentario
+   */
+  function adicionarComentario(){
+    const novaListaComentarios = [
+      ...listaComentarios, 
+      textoComentario 
+    ];
+    setListaComentarios(novaListaComentarios);
+    setTextoComentario("");
+  }
 
   return (
     <main>
@@ -14,14 +34,14 @@ function App() {
         <label htmlFor="comentario">Comentário</label>
 
         <textarea id="comentario" 
-          value = {textoComentario}
-          onChange = {(e) => setTextoComentario(e.target.value)}></textarea>
+          value={textoComentario}
+          onChange={(e) => setTextoComentario(e.target.value)}></textarea>
+
+        <button onClick={adicionarComentario}>Adicionar comentário</button> 
       </section>
 
       <section>
-
         <h2>Comentários adicionados</h2>
-
       </section>
     </main>
   )       
