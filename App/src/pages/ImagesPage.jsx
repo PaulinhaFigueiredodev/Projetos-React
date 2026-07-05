ec98772ad03fcac1675e0465ee9386170248343a
Paula Figueiredo <paulafigueiredo36@gmail.com>
2026-07-04 21:13:16 -0300

    feat: add ImagesPage component for displaying image gallery

---
+++ App/src/pages/ImagesPage.jsx
@@ -0,0 +1,13 @@
+function ImagesPage() {
+
+    return (
+        <main className="images-page">
+            <h1 className="images-page__title">Galeria de imagens</h1>
+            <section className="images-page__section">
+                <h2 className="images-page__subtitle">Lista de imagens</h2>
+            </section>
+        </main>
+    )
+}
+
+export default ImagesPage;
