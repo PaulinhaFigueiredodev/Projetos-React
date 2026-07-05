d3c2e4c05a262218dfa93c51e55f10fb51d56b16
Paula Figueiredo <paulafigueiredo36@gmail.com>
2026-07-04 21:36:37 -0300

    feat: implement routing for CommentsPage and ImagesPage components

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css"
+import { Routes, Route } from "react-router"
 import CommentsPage from "./pages/CommentsPage"
+import ImagesPage from "./pages/ImagesPage"
 
 function App() {
   return (
-    <CommentsPage />
+
+    <Routes>
+      <Route path="/" element={<CommentsPage />} />
+      <Route path="/imagens" element={<ImagesPage />} />
+    </Routes>
   );
 }
 
