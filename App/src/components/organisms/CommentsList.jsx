e6df060130bc86415ff3090a7c98519eca3bccfe
Paula Figueiredo <paulafigueiredo36@gmail.com>
2026-07-06 08:58:36 -0300

    style: format code for consistency and readability across components

--- App/src/components/organisms/CommentsList.jsx
+++ App/src/components/organisms/CommentsList.jsx
@@ -1,28 +1,28 @@
 function CommentsList({ listaComentarios, removerComentario }) {
-    const estaEmDesenvolvimento = (import.meta.env.VITE_ENVIRONMENT === "development");
+	const estaEmDesenvolvimento = (import.meta.env.VITE_ENVIRONMENT === "development");
 
-    return (
-        <section className="comments-list">
-            <h2 className="comments-list__title">Comentários adicionados</h2>
+	return (
+		<section className="comments-list">
+			<h2 className="comments-list__title">Comentários adicionados</h2>
 
-            <ul className="comments-list__items">
-                {listaComentarios.map((comentario, index) => (
-                    <li className="comments-list__item" key={index}>
-                        <p>{comentario}</p>
+			<ul className="comments-list__items">
+				{listaComentarios.map((comentario, index) => (
+					<li className="comments-list__item" key={index}>
+						<p>{comentario}</p>
 
-                        {estaEmDesenvolvimento && (
-                            <button
-                                aria-label={`Remover comentário: ${comentario}`}
-                                className="comments-list__remove-button"
-                                onClick={() => removerComentario(index)}>
-                                Remover comentário
-                            </button>
-                        )}
-                    </li>
-                ))}
-            </ul>
-        </section>
-    );
+						{estaEmDesenvolvimento && (
+							<button
+								aria-label={`Remover comentário: ${comentario}`}
+								className="comments-list__remove-button"
+								onClick={() => removerComentario(index)}>
+								Remover comentário
+							</button>
+						)}
+					</li>
+				))}
+			</ul>
+		</section>
+	);
 }
 
 export default CommentsList;
